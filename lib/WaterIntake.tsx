dce68cfa6ee06794f1d93e0e756353221768e5ed
Alireza177 <alirezamahdavi015@gmail.com>
2024-09-29 14:15:43 +0200

    save waterIntake to AsyncStarage

--- lib/WaterIntake.tsx
+++ lib/WaterIntake.tsx
@@ -1,24 +1,53 @@
-import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function WaterIntake() {
-    const [water, setWater] = useState(0);
+    const [water, setWater] = useState<number>(0);
 
     const addGlass = () => {
-        setWater(water + 1);
+        setWater(prev => {
+            const newWater = prev + 1;
+            storeData('water', newWater.toString());
+            return newWater;
+        });
     };
+
+    const storeData = async (key: string, value: string) => {
+        try {
+            await AsyncStorage.setItem(key, value);
+        } catch (e) {
+            console.error("Failed to save data:", e);
+        }
+    }
+
+    const getData = async (key: string) => {
+        try {
+            const value = await AsyncStorage.getItem(key);
+
+            if (value !== null) {
+                setWater(Number(value));
+            }
+        } catch (e) {
+            console.error("error to fetch data:", e);
+        }
+    }
+
+    useEffect(() => {
+        getData('water');
+    }, []);
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Dagens Vattenintag</Text>
+            <Text style={styles.title}>Today's Water Intake</Text>
             <TextInput
                 style={styles.input}
                 value={String(water)}
                 onChangeText={text => setWater(Number(text))}
                 keyboardType="numeric"
             />
-            <Button title="Lägg till ett glas" onPress={addGlass} />
-            <Text style={{ color: 'white', marginTop: 7 }}>Du har druckit {water} glas vatten idag.</Text>
+            <Button title="Add a glass" onPress={addGlass} />
+            <Text style={{ color: 'white', marginTop: 7 }}>You have drunk {water} glasses of water today.</Text>
         </View>
     );
 }
