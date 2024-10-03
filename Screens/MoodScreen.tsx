10af7bec604fe3f324db8bae109aa3cf87a6f853
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-03 15:33:54 +0200

    add today's mood and workout functionality

--- Screens/MoodScreen.tsx
+++ Screens/MoodScreen.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { user } from "../data";
 
 export default function MoodScreen() {
+
+    const [todaysMood, setTodaysMood] = useState('No mood has been selected');
+
     return (
         <View style={s.container}>
-            <Text style={{ color: "white" }}>Mood: {user.mood}</Text>
+            {user.mood.map((mood, index) => (
+                <TouchableOpacity key={index} onPress={() => setTodaysMood(mood)}>
+                    <Text style={s.infoContainer}>Mood: {mood}</Text>
+                </TouchableOpacity>
+            ))}
+            <Text style={s.selectedMood}>Today's Mood: {todaysMood}</Text>
         </View>
     )
 }
@@ -13,7 +22,20 @@ const s = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        gap: 2,
     },
+    infoContainer: {
+        color: 'white',
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    selectedMood: {
+        color: 'yellow',
+        marginLeft: 20,
+        marginTop: 20,
+        fontSize: 18,
+    }
+
 })
--- Screens/WorkoutScreen.tsx
+++ Screens/WorkoutScreen.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { user } from "../data";
 
 export default function WorkoutScreen() {
+
+    const [todaysWorkout, setTodaysWorkout] = useState('No wprkout have been choosen yet');
+
     return (
         <View style={s.container}>
-            <Text style={{ color: "white" }}>Workout: {user.workouts}</Text>
+            {user.workouts.map((workout, index) => (
+                <TouchableOpacity key={index} onPress={() => setTodaysWorkout(workout)}>
+                    <Text style={s.infoContainer}>Workout: {workout}</Text>
+                </TouchableOpacity>
+            ))}
+            <Text style={s.selectedWorkout}>Today's Workout: {todaysWorkout}</Text>
         </View>
     )
 }
@@ -13,7 +22,20 @@ const s = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        gap: 2,
     },
+    infoContainer: {
+        color: 'white',
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    selectedWorkout: {
+        color: 'yellow',
+        marginLeft: 20,
+        marginTop: 20,
+        fontSize: 18,
+    }
+
 })
